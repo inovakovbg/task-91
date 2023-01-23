5ed2b19bff298ff723aa08857d75fd5df9d79517
Ivan Novakov <inovakov.bg@gmail.com>
2023-01-23 16:16:38 +0200

    Add
    Beats

--- src/js/Application.js
+++ src/js/Application.js
@@ -7,19 +7,48 @@ export default class Application extends EventEmitter {
       READY: "ready",
     };
   }
-
   constructor() {
     super();
 
-    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
-    let count = 0;
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
 
-    document.querySelector(".main").appendChild(message);
+    this._beat = new Beat();
+    this._create();
 
     this.emit(Application.events.READY);
+  }
+
+  _create() {
+    const lyrics = ['Ah', 'ha', 'ha', 'ha', `stayin' alive`, `stayin' alive`];
+    let count = 0;
+    this._beat.addListener(Beat.events.BIT, (bit) => {
+      const message = document.createElement('div');
+      message.classList.add('message');
+      message.textContent = lyrics[count];
+      count++; 
+
+      if (count == 6) {
+        count = 0;
+      }
+      
+      document.querySelector('.main').appendChild(message);
+    })
+
+
+  // constructor() {
+  //   super();
+
+  //   const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+  //   let count = 0;
+
+  //   const message = document.createElement("div");
+  //   message.classList.add("message");
+  //   message.innerText = "Ah";
+
+  //   document.querySelector(".main").appendChild(message);
+
+  //   this.emit(Application.events.READY);
+
+  
   }
 }
